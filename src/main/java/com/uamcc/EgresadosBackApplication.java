c02395531649f054957f12a22d3b60df7a90f2a5
Caleb Hernández <hern98_500@hotmail.com>
2023-02-01 00:31:04 -0600

    first commit

---
+++ src/main/java/com/uamcc/EgresadosBackApplication.java
@@ -0,0 +1,13 @@
+package com.uamcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgresadosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgresadosBackApplication.class, args);
+	}
+
+}
